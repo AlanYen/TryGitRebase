120ea00a23d7c6721a799e8b762835bcaa9ab8c4
AlanYen <alanyen.mobile@gmail.com>
2016-09-01 18:05:10 +0800

    Initial Commit

---
+++ TryGitRebase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryGitRebase
+//
+//  Created by AlanYen on 2016/9/1.
+//  Copyright © 2016年 Alan-App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryGitRebase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryGitRebase
+//
+//  Created by AlanYen on 2016/9/1.
+//  Copyright © 2016年 Alan-App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
